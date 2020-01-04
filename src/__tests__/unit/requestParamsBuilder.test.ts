a935f538b93837d4457fb2dbac0fa53d14349f4c
BeWater <rafa.campoy@gmail.com>
2020-01-04 19:26:05 +0100

    request params unit tests

--- src/__tests__/unit/requestParamsBuilder.test.ts
+++ src/__tests__/unit/requestParamsBuilder.test.ts
@@ -1,5 +1,10 @@
 import { RequestParamsBuilder } from "../../request";
-import { CommandKeyword } from "../../enum";
+import { AggregationType, CommandKeyword } from "../../enum";
+import { Label } from "../../label";
+import { Sample } from "../../sample";
+import { Aggregation } from "../../aggregation";
+import { TimestampRange } from "../../timestampRange";
+import { FilterBuilder } from "../../filter";
 
 let builder: RequestParamsBuilder;
 
@@ -37,4 +42,106 @@ test("add negative retention fails", () => {
     expect(() => {
         builder.addRetention(-100).get();
     }).toThrow(/retention must be positive integer/);
+});
+
+test("add labels", () => {
+    const labels = [new Label("label1", 100), new Label("label2", 200)];
+    expect(builder.addLabels(labels).get()).toEqual([CommandKeyword.LABELS, "label1", 100, "label2", 200]);
+});
+
+test("add empty labels", () => {
+    expect(builder.addLabels([]).get().length).toEqual(0);
+});
+
+test("add no labels", () => {
+    expect(builder.addLabels().get().length).toEqual(0);
+});
+
+test("remove labels", () => {
+    expect(builder.removeLabels().get()).toEqual([CommandKeyword.LABELS]);
+});
+
+test("add samples", () => {
+    const date = Date.now();
+    const samples = [new Sample("sample1", 100, date), new Sample("sample2", 200)];
+    expect(builder.addSamples(samples).get()).toEqual(["sample1", date, 100, "sample2", "*", 200]);
+});
+
+test("add empty samples", () => {
+    expect(builder.addSamples([]).get().length).toEqual(0);
+});
+
+test("add sample", () => {
+    const date = Date.now();
+    expect(builder.addSample(new Sample("sample1", 100, date)).get()).toEqual(["sample1", date, 100]);
+});
+
+test("add sample with optional timestamp", () => {
+    const date = Date.now();
+    expect(builder.addSampleWithOptionalTimeStamp(new Sample("sample1", 100, date)).get()).toEqual([
+        "sample1",
+        100,
+        CommandKeyword.TIMESTAMP,
+        date
+    ]);
+});
+
+test("add count", () => {
+    expect(builder.addCount(7).get()).toEqual([CommandKeyword.COUNT, 7]);
+});
+
+test("add no count", () => {
+    expect(builder.addCount().get().length).toEqual(0);
+});
+
+test("add aggregation", () => {
+    const date = Date.now();
+    expect(builder.addAggregation(new Aggregation(AggregationType.MAX, date)).get()).toEqual([
+        CommandKeyword.AGGREGATION,
+        AggregationType.MAX,
+        date
+    ]);
+});
+
+test("add no aggregation", () => {
+    expect(builder.addAggregation().get().length).toEqual(0);
+});
+
+test("add full range", () => {
+    const date = Date.now();
+    expect(builder.addRange(new TimestampRange(date - 10000, date)).get()).toEqual([date - 10000, date]);
+});
+
+test("add default left range", () => {
+    const date = Date.now();
+    expect(builder.addRange(new TimestampRange(undefined, date)).get()).toEqual([CommandKeyword.MIN_TIMESTAMP, date]);
+});
+
+test("add default right range", () => {
+    const date = Date.now();
+    expect(builder.addRange(new TimestampRange(date - 10000)).get()).toEqual([
+        date - 10000,
+        CommandKeyword.MAX_TIMESTAMP
+    ]);
+});
+
+test("add full default range", () => {
+    expect(builder.addRange(new TimestampRange()).get()).toEqual([
+        CommandKeyword.MIN_TIMESTAMP,
+        CommandKeyword.MAX_TIMESTAMP
+    ]);
+});
+
+test("add filters", () => {
+    const filters = new FilterBuilder("filter1", 100);
+    expect(builder.addFilters(filters).get()).toEqual(["filter1=100"]);
+});
+
+test("add filters with keyword", () => {
+    const filters = new FilterBuilder("filter2", 200);
+    expect(builder.addFiltersWithKeyword(filters).get()).toEqual([CommandKeyword.FILTER, "filter2=200"]);
+});
+
+test("reset", () => {
+    expect(builder.reset().get().length).toEqual(0);
 });
---
+++ src/__tests__/unit/requestParamsDirector.test.ts
@@ -0,0 +1,354 @@
+import { RequestParamsBuilder, RequestParamsDirector } from "../../request";
+import { Label } from "../../label";
+import { AggregationType, CommandKeyword } from "../../enum";
+import { Sample } from "../../sample";
+import { Aggregation } from "../../aggregation";
+import { TimestampRange } from "../../timestampRange";
+import { FilterBuilder } from "../../filter";
+
+let director: RequestParamsDirector;
+let label1: Label;
+let label2: Label;
+let sample1: Sample;
+let sample2: Sample;
+let date: number;
+let aggregation: Aggregation;
+let timestampRange: TimestampRange;
+let filterBuilder: FilterBuilder;
+
+beforeAll(() => {
+    date = Date.now();
+    label1 = new Label("label1", 100);
+    label2 = new Label("label2", 200);
+    sample1 = new Sample("sample1", 10, date - 10000);
+    sample2 = new Sample("sample2", 20, date - 20000);
+    aggregation = new Aggregation(AggregationType.MAX, date);
+    timestampRange = new TimestampRange(date - 20000, date - 10000);
+    filterBuilder = new FilterBuilder("filter", 5);
+});
+
+beforeEach(() => {
+    director = new RequestParamsDirector(new RequestParamsBuilder());
+});
+
+test("create with labels and retention", () => {
+    expect(director.create("key", [label1, label2], 10000).get()).toEqual([
+        "key",
+        CommandKeyword.RETENTION,
+        10000,
+        CommandKeyword.LABELS,
+        label1.getName(),
+        label1.getValue(),
+        label2.getName(),
+        label2.getValue()
+    ]);
+});
+
+test("create without retention", () => {
+    expect(director.create("key", [label1, label2]).get()).toEqual([
+        "key",
+        CommandKeyword.LABELS,
+        label1.getName(),
+        label1.getValue(),
+        label2.getName(),
+        label2.getValue()
+    ]);
+});
+
+test("create without labels", () => {
+    expect(director.create("key", undefined, 10000).get()).toEqual(["key", CommandKeyword.RETENTION, 10000]);
+});
+
+test("create with empty labels", () => {
+    expect(director.create("key", [], 10000).get()).toEqual(["key", CommandKeyword.RETENTION, 10000]);
+});
+
+test("create default", () => {
+    expect(director.create("key").get()).toEqual(["key"]);
+});
+
+test("alter with labels and retention", () => {
+    expect(director.alter("key", [label1, label2], 10000).get()).toEqual([
+        "key",
+        CommandKeyword.RETENTION,
+        10000,
+        CommandKeyword.LABELS,
+        label1.getName(),
+        label1.getValue(),
+        label2.getName(),
+        label2.getValue()
+    ]);
+});
+
+test("alter without retention", () => {
+    expect(director.alter("key", [label1, label2]).get()).toEqual([
+        "key",
+        CommandKeyword.LABELS,
+        label1.getName(),
+        label1.getValue(),
+        label2.getName(),
+        label2.getValue()
+    ]);
+});
+
+test("alter without labels", () => {
+    expect(director.alter("key", undefined, 10000).get()).toEqual(["key", CommandKeyword.RETENTION, 10000]);
+});
+
+test("alter with empty labels", () => {
+    expect(director.alter("key", [], 10000).get()).toEqual([
+        "key",
+        CommandKeyword.RETENTION,
+        10000,
+        CommandKeyword.LABELS
+    ]);
+});
+
+test("alter default", () => {
+    expect(director.alter("key").get()).toEqual(["key"]);
+});
+
+test("add with labels and retention", () => {
+    expect(director.add(sample1, [label1, label2], 10000).get()).toEqual([
+        "sample1",
+        date - 10000,
+        10,
+        CommandKeyword.RETENTION,
+        10000,
+        CommandKeyword.LABELS,
+        label1.getName(),
+        label1.getValue(),
+        label2.getName(),
+        label2.getValue()
+    ]);
+});
+
+test("add without retention", () => {
+    expect(director.add(sample1, [label1, label2]).get()).toEqual([
+        "sample1",
+        date - 10000,
+        10,
+        CommandKeyword.LABELS,
+        label1.getName(),
+        label1.getValue(),
+        label2.getName(),
+        label2.getValue()
+    ]);
+});
+
+test("add without labels", () => {
+    expect(director.add(sample1, undefined, 10000).get()).toEqual([
+        "sample1",
+        date - 10000,
+        10,
+        CommandKeyword.RETENTION,
+        10000
+    ]);
+});
+
+test("add with empty labels", () => {
+    expect(director.add(sample1, [], 10000).get()).toEqual([
+        "sample1",
+        date - 10000,
+        10,
+        CommandKeyword.RETENTION,
+        10000
+    ]);
+});
+
+test("create default", () => {
+    expect(director.add(sample1).get()).toEqual(["sample1", date - 10000, 10]);
+});
+
+test("multiadd", () => {
+    expect(director.multiAdd([sample1, sample2]).get()).toEqual([
+        "sample1",
+        date - 10000,
+        10,
+        "sample2",
+        date - 20000,
+        20
+    ]);
+});
+
+test("multiadd with empty labels", () => {
+    expect(director.multiAdd([]).get().length).toEqual(0);
+});
+
+test("change by with labels and retention", () => {
+    expect(director.changeBy(sample1, [label1, label2], 10000).get()).toEqual([
+        "sample1",
+        10,
+        CommandKeyword.TIMESTAMP,
+        date - 10000,
+        CommandKeyword.RETENTION,
+        10000,
+        CommandKeyword.LABELS,
+        label1.getName(),
+        label1.getValue(),
+        label2.getName(),
+        label2.getValue()
+    ]);
+});
+
+test("change by without retention", () => {
+    expect(director.changeBy(sample1, [label1, label2]).get()).toEqual([
+        "sample1",
+        10,
+        CommandKeyword.TIMESTAMP,
+        date - 10000,
+        CommandKeyword.LABELS,
+        label1.getName(),
+        label1.getValue(),
+        label2.getName(),
+        label2.getValue()
+    ]);
+});
+
+test("change by without labels", () => {
+    expect(director.changeBy(sample1, undefined, 10000).get()).toEqual([
+        "sample1",
+        10,
+        CommandKeyword.TIMESTAMP,
+        date - 10000,
+        CommandKeyword.RETENTION,
+        10000
+    ]);
+});
+
+test("change by with empty labels", () => {
+    expect(director.changeBy(sample1, [], 10000).get()).toEqual([
+        "sample1",
+        10,
+        CommandKeyword.TIMESTAMP,
+        date - 10000,
+        CommandKeyword.RETENTION,
+        10000
+    ]);
+});
+
+test("change by default", () => {
+    expect(director.changeBy(sample1).get()).toEqual(["sample1", 10, CommandKeyword.TIMESTAMP, date - 10000]);
+});
+
+test("create rule", () => {
+    expect(director.createRule("source", "destination", aggregation).get()).toEqual([
+        "source",
+        "destination",
+        CommandKeyword.AGGREGATION,
+        AggregationType.MAX,
+        date
+    ]);
+});
+
+test("create rule with same source and destination fails", () => {
+    expect(() => {
+        director.createRule("key", "key", aggregation);
+    }).toThrow(/source and destination key cannot be equals/);
+});
+
+test("delete rule", () => {
+    expect(director.deleteRule("source", "destination").get()).toEqual(["source", "destination"]);
+});
+
+test("delete rule with same source and destination fails", () => {
+    expect(() => {
+        director.deleteRule("key", "key");
+    }).toThrow(/source and destination key cannot be equals/);
+});
+
+test("range with count and aggregation", () => {
+    expect(director.range("key", timestampRange, 3, aggregation).get()).toEqual([
+        "key",
+        date - 20000,
+        date - 10000,
+        CommandKeyword.COUNT,
+        3,
+        CommandKeyword.AGGREGATION,
+        AggregationType.MAX,
+        date
+    ]);
+});
+
+test("range without aggregation", () => {
+    expect(director.range("key", timestampRange, 3).get()).toEqual([
+        "key",
+        date - 20000,
+        date - 10000,
+        CommandKeyword.COUNT,
+        3
+    ]);
+});
+
+test("range without count", () => {
+    expect(director.range("key", timestampRange, undefined, aggregation).get()).toEqual([
+        "key",
+        date - 20000,
+        date - 10000,
+        CommandKeyword.AGGREGATION,
+        AggregationType.MAX,
+        date
+    ]);
+});
+
+test("range default", () => {
+    expect(director.range("key", timestampRange).get()).toEqual(["key", date - 20000, date - 10000]);
+});
+
+test("multi range with count and aggregation", () => {
+    expect(director.multiRange(timestampRange, filterBuilder, 3, aggregation).get()).toEqual([
+        date - 20000,
+        date - 10000,
+        CommandKeyword.COUNT,
+        3,
+        CommandKeyword.AGGREGATION,
+        AggregationType.MAX,
+        date,
+        CommandKeyword.FILTER,
+        "filter=5"
+    ]);
+});
+
+test("multi range without aggregation", () => {
+    expect(director.multiRange(timestampRange, filterBuilder, 3).get()).toEqual([
+        date - 20000,
+        date - 10000,
+        CommandKeyword.COUNT,
+        3,
+        CommandKeyword.FILTER,
+        "filter=5"
+    ]);
+});
+
+test("multi range without count", () => {
+    expect(director.multiRange(timestampRange, filterBuilder, undefined, aggregation).get()).toEqual([
+        date - 20000,
+        date - 10000,
+        CommandKeyword.AGGREGATION,
+        AggregationType.MAX,
+        date,
+        CommandKeyword.FILTER,
+        "filter=5"
+    ]);
+});
+
+test("multi range default", () => {
+    expect(director.multiRange(timestampRange, filterBuilder).get()).toEqual([
+        date - 20000,
+        date - 10000,
+        CommandKeyword.FILTER,
+        "filter=5"
+    ]);
+});
+
+test("get key", () => {
+    expect(director.getKey("key").get()).toEqual(["key"]);
+});
+
+test("multi get", () => {
+    expect(director.multiGet(filterBuilder).get()).toEqual([CommandKeyword.FILTER, "filter=5"]);
+});
+
+test("query index", () => {
+    expect(director.queryIndex(filterBuilder).get()).toEqual(["filter=5"]);
+});
